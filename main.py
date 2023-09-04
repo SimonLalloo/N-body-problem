55bf0075091bced6d60c2440a2fa93637da7d21f
SimonLalloo <simon@lalloo.net>
2023-09-04 21:25:28 +0200

    Improve simulation

--- main.py
+++ main.py
@@ -1,37 +1,10 @@
 import numpy as np
 import scipy as sp
 
-# Test stuff
-def print_values():
-    print("file")
-    print(file)
-
-    print("x")
-    print(position_x)
-
-    print("y")
-    print(position_y)
-
-    print("mass")
-    print(mass)
-
-    print("v_x")
-    print(velocity_x)
-
-    print("v_y")
-    print(velocity_y)
-
-    print("f_x")
-    print(forces_x)
-
-    print("f_y")
-    print(forces_y)
-
-
-
 # This is a vector
 # This is the 'thick' r from the pdf
 def distance(i, j):
+    # print("i and j are", i, j)
     x = (position_x[i] - position_x[j])
     y = (position_y[i] - position_y[j])
     return np.array([x, y])
@@ -39,32 +12,57 @@ def distance(i, j):
 
 
 # This is the 'thin' r from the pdf
-def r(i, j):
-    x = (position_x[i] - position_x[j])
-    y = (position_y[i] - position_y[j])
-    return np.sqrt(x ** 2 + y ** 2)
+def r(distance):
+    return np.sqrt(distance[0] ** 2 + distance[1] ** 2)
 
 
 
-def calculate_force(i, j):
-    return -G * mass[i] * mass[j] * distance(i, j) / (r(i, j) + epsilon) ** 3
+def calc_force(i, j):
+    dist = distance(i,j)
+    forces[i][j] = -G * mass[i] * mass[j] * dist / (r(dist) + epsilon) ** 3
 
 
 
 def calc_forces():
     for i in range (0, particle_count - 1):
-        for j in range (i, particle_count):
-            # Calculate force
-            (force_x, force_y) = calculate_force(i, j)
-            forces_x[i][j] = force_x
-            forces_y[i][j] = force_y
+        for j in range (i + 1, particle_count):
+            force = calc_force(i, j)
+
+
+
+def acceleration(i, force):
+    return force / mass[i]
+
+
+
+def update_velocity(i, force):
+    delta_v = timestep * acceleration(i, force)
+    velocity_x[i] = velocity_x[i] + delta_v[0]
+    velocity_y[i] = velocity_y[i] + delta_v[0]
+
+
+
+def update_position(i):
+    position_x[i] = position_x[i] + timestep * velocity_x[i]
+    position_y[i] = position_y[i] + timestep * velocity_y[i]
 
 
 
 def move_particles():
-    # Calculate total force on a particle
-    # Calculate particle velocity
-    # Update particle position
+    for i in range (0, particle_count):
+        total_force = 0
+
+        for j in range (0, particle_count):
+            if i == j:
+                continue
+            elif i < j:
+                total_force -= forces[i][j]
+            else :
+                total_force += forces[j][i]
+
+        print("Particle", i, "has total force", total_force)
+        update_velocity(i, total_force)
+        update_position(i)
 
 
 
@@ -73,9 +71,7 @@ def move_particles():
 # Init stuff
 path = "/home/simon/repos/N-body-problem/Nbody/Nbody/input_data/circles_N_2.gal"
 file = np.fromfile(path, dtype=float)
-steps = 2
-
-# TODO: look at (reshape array)
+steps = 200
 
 position_x  = file[0::6]
 position_y  = file[1::6]
@@ -91,20 +87,23 @@ G = 100 / particle_count
 timestep = 10 ** -5
 
 # Array of all forces
-forces_x    = np.zeros((particle_count, particle_count))
-forces_y    = np.zeros((particle_count, particle_count))
+forces = np.zeros((particle_count, particle_count, 2))
+
+print("Step number 0")
+print("X coords", position_x)
+print("Y coords", position_y)
 
 for step in range(1, steps + 1):
-    print("Step number:")
-    print(step)
+    print("\n\n\nStep number", step)
 
-    # Calculate the forces between each particle pair
+    # Calculate the forces between each pair of particles
     calc_forces()
-    # Calculate the total force & velocity, and move the particle
+    # Calculate the total force & velocity, then move the particle
     move_particles()
 
-    print(forces_x)
-    # print_values()
+    # print("forces ", forces)
+    print("X coords", position_x)
+    print("Y coords", position_y)
 
 # Put stuff into file
 file.tofile("sol_N_2.gal")
